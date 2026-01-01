bc419bffae1287272ad5889ff47ee1f0d8a54543
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-01 12:58:12 +0300

    kernel 45 - `main scripts` (45/4865)

--- test/test.py
+++ test/test.py
@@ -42,3 +42,4 @@ try:
     _mistral_import_error_msg = ""
 except ImportError:
     _MISTRAL_COMMON_DATASET_MEAN = (0.48145466, 0.4578275, 0.40821073)
+    _MISTRAL_COMMON_DATASET_STD = (0.26862954, 0.26130258, 0.27577711)
