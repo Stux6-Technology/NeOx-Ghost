fdd5e742e58fe85575b105cf2fa6af69decb16e6
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-06 11:46:41 +0300

    kernel 289 - `main scripts` (289/4865)

--- test/test.py
+++ test/test.py
@@ -286,3 +286,4 @@ class ModelBase:
                     for i, size in enumerate(block_size):
                         scale = scale.repeat_interleave(size, i)
                     # unpad the scale (e.g. when the tensor size isn't a multiple of the block size)
+                    scale = scale[tuple(slice(0, size) for size in weight.shape)]
