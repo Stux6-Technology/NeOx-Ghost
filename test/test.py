06c4f5aaf4f0f198fc396a878ff3598f257f92fe
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-08 14:58:23 +0300

    kernel 396 - `main scripts` (396/4865)

--- test/test.py
+++ test/test.py
@@ -393,3 +393,4 @@ class ModelBase:
                         s = self.model_tensors[name]
                         self.model_tensors[weight_name] = lambda w=w, s=s, bs=block_size: dequant_simple(w(), s(), bs)
                         tensors_to_remove.append(name)
+                    if name.endswith(".qscale_act"):
