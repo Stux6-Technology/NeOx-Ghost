4d903bb5ed6fb2a3b1ed375d5e9ab06364ed5dab
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-09 17:09:42 +0300

    kernel 462 - `main scripts` (462/4865)

--- test/test.py
+++ test/test.py
@@ -459,3 +459,4 @@ class ModelBase:
                             if (base_name + "_zero_point") in self.model_tensors:
                                 tensors_to_remove.append(base_name + "_zero_point")
                 else:
+                    raise NotImplementedError(f"Quant format {quant_format!r} for method {quant_method!r} is not yet supported")
