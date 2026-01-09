08b4f413f4aa94c7780a9676136f1734c0e69b68
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-09 15:49:59 +0300

    kernel 487 - `main scripts` (487/4865)

--- test/test.py
+++ test/test.py
@@ -484,3 +484,4 @@ class ModelBase:
         return name + suffix
 
     def match_model_tensor_name(self, name: str, key: gguf.MODEL_TENSOR, bid: int | None, suffix: str = ".weight") -> bool:
+        if key not in gguf.MODEL_TENSORS[self.model_arch]:
