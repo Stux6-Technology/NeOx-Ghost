2d4465bf1b654a9f6ac475150fd868553a0f1943
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-11 23:09:15 +0300

    kernel 604 - `main scripts` (604/4865)

--- test/test.py
+++ test/test.py
@@ -601,3 +601,4 @@ class ModelBase:
 
                 # No override (data_qtype is False), or wants to be quantized (data_qtype is True)
                 if isinstance(data_qtype, bool):
+                    if self.ftype == gguf.LlamaFileType.ALL_F32:
