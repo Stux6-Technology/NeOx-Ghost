b02e56cf6a8655b63f6493b7e7488430df0e7886
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-14 11:19:51 +0300

    kernel 771 - `main scripts` (771/4865)

--- test/test.py
+++ test/test.py
@@ -768,3 +768,4 @@ class TextModel(ModelBase):
             if "rope_theta" not in self.rope_parameters and (rope_theta := self.find_hparam(["rope_theta", "global_rope_theta", "rotary_emb_base"], optional=True)) is not None:
                 self.rope_parameters["rope_theta"] = rope_theta
             if "rope_type" not in self.rope_parameters and (rope_type := self.rope_parameters.get("type")) is not None:
+                self.rope_parameters["rope_type"] = rope_type
