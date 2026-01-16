3903ae8ac8eaaa665b420922de471fc709e40f8a
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-16 16:47:15 +0300

    kernel 884 - `main scripts` (884/4865)

--- test/test.py
+++ test/test.py
@@ -881,3 +881,4 @@ class TextModel(ModelBase):
             self.gguf_writer.add_layer_norm_rms_eps(f_rms_eps)
             logger.info(f"gguf: rms norm epsilon = {f_rms_eps}")
         if (f_norm_eps := self.find_hparam(["layer_norm_eps", "layer_norm_epsilon", "norm_epsilon"], optional=True)) is not None:
+            self.gguf_writer.add_layer_norm_eps(f_norm_eps)
