f153322066a2376f74aac8fcb98c0a8c5ad63a3e
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-16 11:32:34 +0300

    kernel 848 - `main scripts` (848/4865)

--- test/test.py
+++ test/test.py
@@ -845,3 +845,4 @@ class TextModel(ModelBase):
                 self.gguf_writer.add_rope_scaling_factor(rope_factor)
                 self.gguf_writer.add_rope_scaling_orig_ctx_len(rope_params["original_max_position_embeddings"])
                 if (yarn_ext_factor := rope_params.get("extrapolation_factor")) is not None:
+                    self.gguf_writer.add_rope_scaling_yarn_ext_factor(yarn_ext_factor)
