3aefcaade76690b297ae17327baaf731cf5c5f35
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-16 09:33:06 +0300

    kernel 891 - `main scripts` (891/4865)

--- test/test.py
+++ test/test.py
@@ -888,3 +888,4 @@ class TextModel(ModelBase):
             logger.info(f"gguf: expert count = {n_experts}")
         if (n_experts_used := self.hparams.get("num_experts_per_tok")) is not None:
             self.gguf_writer.add_expert_used_count(n_experts_used)
+            logger.info(f"gguf: experts used count = {n_experts_used}")
