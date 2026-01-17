f355c34b5606f4698e1e8f7e7352b20c50e8867b
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-17 20:34:39 +0300

    kernel 913 - `main scripts` (913/4865)

--- test/test.py
+++ test/test.py
@@ -910,3 +910,4 @@ class TextModel(ModelBase):
             self.gguf_writer.add_value_length(head_dim)
 
         self.gguf_writer.add_file_type(self.ftype)
+        logger.info(f"gguf: file type = {self.ftype}")
