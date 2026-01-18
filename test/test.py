fe36dea4cfc1fb37b250ae6f9e07a47181fb2266
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-18 22:22:40 +0300

    kernel 1014 - `main scripts` (1014/4865)

--- test/test.py
+++ test/test.py
@@ -1011,3 +1011,4 @@ class TextModel(ModelBase):
 
         # NOTE: if you get an error here, you need to update the convert_hf_to_gguf_update.py script
         #       or pull the latest version of the model from Huggingface
+        #       don't edit the hashes manually!
