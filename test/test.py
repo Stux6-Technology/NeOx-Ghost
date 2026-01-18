a40336ab81da8a66469df4cf70ec548154e7c14b
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-18 10:28:57 +0300

    kernel 967 - `main scripts` (967/4865)

--- test/test.py
+++ test/test.py
@@ -964,3 +964,4 @@ class TextModel(ModelBase):
         for i in range(vocab_size):
             if i not in reverse_vocab:
                 tokens.append(f"[PAD{i}]")
+                toktypes.append(gguf.TokenType.UNUSED)
