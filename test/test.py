e20f9046c2f2f855588f9ab9a1f6771a936d1683
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-18 11:22:35 +0300

    kernel 962 - `main scripts` (962/4865)

--- test/test.py
+++ test/test.py
@@ -959,3 +959,4 @@ class TextModel(ModelBase):
         reverse_vocab = {id_: encoded_tok for encoded_tok, id_ in tokenizer.vocab.items()}
         added_vocab = tokenizer.get_added_vocab()
 
+        added_tokens_decoder = tokenizer.added_tokens_decoder
