e063d9f974e3945e03776d1e22bcab81a81dc420
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-18 19:17:52 +0300

    kernel 973 - `main scripts` (973/4865)

--- test/test.py
+++ test/test.py
@@ -970,3 +970,4 @@ class TextModel(ModelBase):
                 if token in added_vocab:
                     # The tokenizer in llama.cpp assumes the CONTROL and USER_DEFINED tokens are pre-normalized.
                     # To avoid unexpected issues - we make sure to normalize non-normalized tokens
+                    if not added_tokens_decoder[i].normalized:
