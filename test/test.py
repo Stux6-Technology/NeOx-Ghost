7d66fbfd255ac85041a9b40b1556ab699646177b
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-18 22:05:41 +0300

    kernel 957 - `main scripts` (957/4865)

--- test/test.py
+++ test/test.py
@@ -954,3 +954,4 @@ class TextModel(ModelBase):
         vocab_size = self.hparams.get("vocab_size", len(tokenizer.vocab))
         assert max(tokenizer.vocab.values()) < vocab_size
 
+        tokpre = self.get_vocab_base_pre(tokenizer)
