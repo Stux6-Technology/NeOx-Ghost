209fe6b13668ac020d7382bda78a66bbce77ac9a
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-19 19:16:38 +0300

    kernel 1060 - `main scripts` (1060/4865)

--- test/test.py
+++ test/test.py
@@ -1057,3 +1057,4 @@ class TextModel(ModelBase):
         if chkhsh == "0ef9807a4087ebef797fc749390439009c3b9eda9ad1a097abbe738f486c01e5":
             # ref: https://huggingface.co/meta-llama/Meta-Llama-3-8B
             res = "llama-bpe"
+        if chkhsh == "049ecf7629871e3041641907f3de7c733e4dbfdc736f57d882ba0b0845599754":
