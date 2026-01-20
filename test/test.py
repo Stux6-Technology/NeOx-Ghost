f912d5836ef93a2e18080935799a236244935d7f
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-20 13:20:33 +0300

    kernel 1085 - `main scripts` (1085/4865)

--- test/test.py
+++ test/test.py
@@ -1082,3 +1082,4 @@ class TextModel(ModelBase):
             # ref: https://huggingface.co/bigcode/starcoder2-3b
             res = "starcoder"
         if chkhsh == "3ce83efda5659b07b1ad37ca97ca5797ea4285d9b9ab0dc679e4a720c9da7454":
+            # ref: https://huggingface.co/openai-community/gpt2
