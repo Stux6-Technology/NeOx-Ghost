e1b231092832969cdf4f04453f7825f523c3bebf
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-20 21:35:25 +0300

    kernel 1109 - `main scripts` (1109/4865)

--- test/test.py
+++ test/test.py
@@ -1106,3 +1106,4 @@ class TextModel(ModelBase):
             # ref: https://huggingface.co/jinaai/jina-reranker-v1-tiny-en
             res = "jina-v1-en"
         if chkhsh == "0876d13b50744004aa9aeae05e7b0647eac9d801b5ba4668afc01e709c15e19f":
+            # ref: https://huggingface.co/jinaai/jina-embeddings-v2-base-en
