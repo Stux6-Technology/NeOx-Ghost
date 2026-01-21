64f0993dfefb60d7081996fc16a973571d35f20e
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-21 18:05:07 +0300

    kernel 1201 - `main scripts` (1201/4865)

--- test/test.py
+++ test/test.py
@@ -1198,3 +1198,4 @@ class TextModel(ModelBase):
         if chkhsh == "169bf0296a13c4d9b7672313f749eb36501d931022de052aad6e36f2bf34dd51":
             # ref: https://huggingface.co/LiquidAI/LFM2-Tokenizer
             res = "lfm2"
+        if chkhsh == "2085e1638f6c377a0aa4ead21b27bb4cb941bf800df86ed391011769c1758dfb":
