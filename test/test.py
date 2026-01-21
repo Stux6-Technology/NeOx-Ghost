d36018922eda6b11b4f2d11ea411ff2aa39bf106
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-21 09:40:22 +0300

    kernel 1168 - `main scripts` (1168/4865)

--- test/test.py
+++ test/test.py
@@ -1165,3 +1165,4 @@ class TextModel(ModelBase):
         if chkhsh == "877081d19cf6996e2c4ff0e1236341e9b7bde288f5311a56a937f0afbbb3aeb5":
             # ref: https://huggingface.co/deepseek-ai/DeepSeek-V3
             res = "deepseek-v3"
+        if chkhsh == "b3f499bb4255f8ca19fccd664443283318f2fd2414d5e0b040fbdd0cc195d6c5":
