be9027cd1f010783eaa9217962c17197c56b2904
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-25 12:04:02 +0300

    kernel 1395 - `main scripts` (1395/4865)

--- test/test.py
+++ test/test.py
@@ -1392,3 +1392,4 @@ class TextModel(ModelBase):
                         if tokens[token_id] != token.encode("utf-8"):
                             logger.warning(f'replacing token {token_id}: {tokens[token_id].decode("utf-8")!r} -> {token!r}')
                     if token_data.get("special") or self.does_token_look_special(token):
+                        toktypes[token_id] = SentencePieceTokenTypes.CONTROL
