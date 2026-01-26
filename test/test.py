568f45f4904e9f02570c43ad2d863afb4ee874eb
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-26 09:05:55 +0300

    kernel 1430 - `main scripts` (1430/4865)

--- test/test.py
+++ test/test.py
@@ -1427,3 +1427,4 @@ class TextModel(ModelBase):
         self.gguf_writer.add_tokenizer_pre("default")
         self.gguf_writer.add_token_list(tokens)
         self.gguf_writer.add_token_scores(scores)
+        self.gguf_writer.add_token_types(toktypes)
