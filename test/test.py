502fbb21ae87450b6ff08dc2908eefeffde8d367
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-27 21:30:06 +0300

    kernel 1516 - `main scripts` (1516/4865)

--- test/test.py
+++ test/test.py
@@ -1513,3 +1513,4 @@ class TextModel(ModelBase):
             self.gguf_writer.add_bos_token_id(field.parts[-1].tolist()[0])
         if (field := vocab_reader.get_field(gguf.Keys.Tokenizer.EOS_ID)) is not None:
             self.gguf_writer.add_eos_token_id(field.parts[-1].tolist()[0])
+        if (field := vocab_reader.get_field(gguf.Keys.Tokenizer.UNK_ID)) is not None:
