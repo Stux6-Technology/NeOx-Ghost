4c24cfd92469b58f680284a3da7bd0c44bfe1982
Alperen ERKAN <erkanalperen54@gmail.com>
2026-01-27 10:37:29 +0300

    kernel 1463 - `main scripts` (1463/4865)

--- test/test.py
+++ test/test.py
@@ -1460,3 +1460,4 @@ class TextModel(ModelBase):
         self.gguf_writer.add_tokenizer_model("rwkv")
         self.gguf_writer.add_token_list(tokens)
         self.gguf_writer.add_token_types(toktypes)
+        special_vocab = gguf.SpecialVocab(self.dir_model, load_merges=False)
