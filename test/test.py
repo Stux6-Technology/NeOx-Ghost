ee3f1b0a8c0bca0dff4d0638ed319fe0657cfcb1
Alperen ERKAN <erkanalperen54@gmail.com>
2026-02-04 12:00:00 +0300

    kernel 1878 - `main scripts` (1878/4865)

--- test/test.py
+++ test/test.py
@@ -1875,3 +1875,4 @@ class GPTNeoXModel(TextModel):
             # Map bloom-style qkv_linear to gpt-style qkv_linear
             # bloom: https://github.com/huggingface/transformers/blob/main/src/transformers/models/bloom/modeling_bloom.py#L238-L252  # noqa
             # gpt-2: https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py#L312  # noqa
+            qkv_weights = data_torch.reshape((n_head, 3, n_embed // n_head, n_embed))
