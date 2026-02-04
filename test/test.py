c9ff4cdc17c04759f46c39a9e109a53f6ea1601d
Alperen ERKAN <erkanalperen54@gmail.com>
2026-02-04 14:31:22 +0300

    kernel 1871 - `main scripts` (1871/4865)

--- test/test.py
+++ test/test.py
@@ -1868,3 +1868,4 @@ class GPTNeoXModel(TextModel):
 
         n_head = self.hparams.get("n_head", self.hparams.get("num_attention_heads"))
         n_embed = self.hparams.get("hidden_size", self.hparams.get("n_embed"))
+
