af18c8329d045dcef771700bd4ea7fe95ba8bc72
Alperen ERKAN <erkanalperen54@gmail.com>
2026-02-08 10:13:14 +0300

    kernel 2061 - `main scripts` (2061/4865)

--- test/test.py
+++ test/test.py
@@ -2058,3 +2058,4 @@ class BaichuanModel(TextModel):
         if bid is not None and name == f"model.layers.{bid}.self_attn.W_pack.weight":
             logger.info(f"Unpacking and permuting layer {bid}")
             tensors = [
+                (self.format_tensor_name(gguf.MODEL_TENSOR.ATTN_Q, bid),
