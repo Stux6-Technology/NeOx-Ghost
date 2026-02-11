1c5a27ab4f20327633e68de6fb3c284350ae9009
Alperen ERKAN <erkanalperen54@gmail.com>
2026-02-11 20:58:00 +0300

    kernel 2195 - `main scripts` (2195/4865)

--- test/test.py
+++ test/test.py
@@ -2192,3 +2192,4 @@ class FalconModel(TextModel):
         self.gguf_writer.add_feed_forward_length(4 * self.hparams["hidden_size"])
         self.gguf_writer.add_block_count(self.block_count)
         self.gguf_writer.add_head_count(n_head)
+        self.gguf_writer.add_head_count_kv(n_head_kv)
