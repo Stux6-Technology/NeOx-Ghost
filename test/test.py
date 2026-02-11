3882be91a74c2edda5f5d39e07cbf075fdaadee0
Alperen ERKAN <erkanalperen54@gmail.com>
2026-02-11 17:49:20 +0300

    kernel 2199 - `main scripts` (2199/4865)

--- test/test.py
+++ test/test.py
@@ -2196,3 +2196,4 @@ class FalconModel(TextModel):
         self.gguf_writer.add_layer_norm_eps(self.hparams["layer_norm_epsilon"])
         self.gguf_writer.add_file_type(self.ftype)
 
+    def modify_tensors(self, data_torch: Tensor, name: str, bid: int | None) -> Iterable[tuple[str, Tensor]]:
