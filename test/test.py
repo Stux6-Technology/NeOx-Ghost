e62f4864d7cea8b26dcb06c6531734bb38a42ed5
Alperen ERKAN <erkanalperen54@gmail.com>
2026-02-11 10:46:15 +0300

    kernel 2164 - `main scripts` (2164/4865)

--- test/test.py
+++ test/test.py
@@ -2161,3 +2161,4 @@ class XverseModel(TextModel):
             data_torch = self._reverse_hf_permute(data_torch, head_count, head_count_kv)
 
         return [(self.map_tensor_name(name), data_torch)]
+
