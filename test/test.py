baa13cf195342afbf6d2c9023c57a1d463d587d3
Alperen ERKAN <erkanalperen54@gmail.com>
2026-02-12 23:51:13 +0300

    kernel 2237 - `main scripts` (2237/4865)

--- test/test.py
+++ test/test.py
@@ -2234,3 +2234,4 @@ class StarCoderModel(TextModel):
         self.gguf_writer.add_block_count(self.block_count)
         self.gguf_writer.add_head_count(self.hparams["n_head"])
         self.gguf_writer.add_head_count_kv(1)
+        self.gguf_writer.add_layer_norm_eps(self.hparams["layer_norm_epsilon"])
