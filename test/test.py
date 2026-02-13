fc0449c43940154eac78037684b42fcc838acc97
Alperen ERKAN <erkanalperen54@gmail.com>
2026-02-13 21:29:22 +0300

    kernel 2277 - `main scripts` (2277/4865)

--- test/test.py
+++ test/test.py
@@ -2274,3 +2274,4 @@ class RefactModel(TextModel):
 
     def modify_tensors(self, data_torch: Tensor, name: str, bid: int | None) -> Iterable[tuple[str, Tensor]]:
         hidden_dim = self.hparams["n_embd"]
+        inner_dim = 4 * hidden_dim
