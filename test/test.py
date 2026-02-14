3ca1bd13a6d4549c31491be0c5f00aff8fd22e43
Alperen ERKAN <erkanalperen54@gmail.com>
2026-02-14 17:49:02 +0300

    kernel 2359 - `main scripts` (2359/4865)

--- test/test.py
+++ test/test.py
@@ -2356,3 +2356,4 @@ class StableLMModel(TextModel):
 
             if len(self._k_norms[bid]) >= n_kv_head:
                 return self._stack_qk_norm(bid, n_kv_head, self._k_norms[bid], "k_layernorm")
+            else:
