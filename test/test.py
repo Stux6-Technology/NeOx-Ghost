7727278dd14f7459b7499cbcc5a5976e385ccb3d
Alperen ERKAN <erkanalperen54@gmail.com>
2026-02-14 09:35:45 +0300

    kernel 2355 - `main scripts` (2355/4865)

--- test/test.py
+++ test/test.py
@@ -2352,3 +2352,4 @@ class StableLMModel(TextModel):
             if self._k_norms is None:
                 self._k_norms = [{} for _ in range(self.block_count)]
 
+            self._k_norms[bid][name] = data_torch
