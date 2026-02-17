2b961a03f5cce471c23256ecaece5198ebeeda25
Alperen ERKAN <erkanalperen54@gmail.com>
2026-02-17 17:04:47 +0300

    kernel 2466 - `main scripts` (2466/4865)

--- test/test.py
+++ test/test.py
@@ -2463,3 +2463,4 @@ class LlamaModel(TextModel):
 
         if (rope_dim := hparams.get("head_dim")) is None:
             rope_dim = hparams["hidden_size"] // hparams["num_attention_heads"]
+        self.gguf_writer.add_rope_dimension_count(rope_dim)
