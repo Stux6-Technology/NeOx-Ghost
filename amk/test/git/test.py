cb0e8156c79890316df4c8fc4a9729ebf2c0d7f9
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-10 22:57:44 +0300

    amk agent dev. sys. 508 - `tester agent scripts/general agents` (508/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -505,3 +505,4 @@ class ModelBase:
     def set_gguf_parameters(self):
         raise NotImplementedError("set_gguf_parameters() must be implemented in subclasses")
 
+    def modify_tensors(self, data_torch: Tensor, name: str, bid: int | None) -> Iterable[tuple[str, Tensor]]:
