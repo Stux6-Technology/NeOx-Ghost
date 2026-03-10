90a0be0341764b8b2e23d26d303f2b696f05cfba
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-10 10:11:15 +0300

    amk agent dev. sys. 451 - `tester agent scripts/general agents` (451/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -448,3 +448,4 @@ class ModelBase:
                             base_name = name.removesuffix("_packed")
                             w = self.model_tensors[name]
                             scale = self.model_tensors[base_name + "_scale"]
+                            shape = self.model_tensors[base_name + "_shape"]
