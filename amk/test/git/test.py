039c103803678e7d9db3dcc91770a3e78eefceb5
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-20 14:43:56 +0300

    amk agent dev. sys. 943 - `tester agent scripts/general agents` (943/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -940,3 +940,4 @@ class TextModel(ModelBase):
         seems_special = seems_special or (token_text.startswith("<｜") and token_text.endswith("｜>"))  # deepseek-coder
 
         # TODO: should these be marked as UNUSED instead? (maybe not)
+        seems_special = seems_special or (token_text.startswith("<unused") and token_text.endswith(">"))  # gemma{,-2}
