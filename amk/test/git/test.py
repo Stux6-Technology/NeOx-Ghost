917e03cd5d09d66fe7d102535d5c1c1b5e636f59
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-21 22:44:02 +0300

    amk agent dev. sys. 1059 - `tester agent scripts/general agents` (1059/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1056,3 +1056,4 @@ class TextModel(ModelBase):
             res = "grok-2"
         if chkhsh == "0ef9807a4087ebef797fc749390439009c3b9eda9ad1a097abbe738f486c01e5":
             # ref: https://huggingface.co/meta-llama/Meta-Llama-3-8B
+            res = "llama-bpe"
