ddd677f8c69b154238fd92589a052ffcd4012e58
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-24 22:42:43 +0300

    amk agent dev. sys. 1218 - `tester agent scripts/general agents` (1218/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1215,3 +1215,4 @@ class TextModel(ModelBase):
             res = "granite-docling"
         if chkhsh == "f4f37b6c8eb9ea29b3eac6bb8c8487c5ab7885f8d8022e67edc1c68ce8403e95":
             # ref: https://huggingface.co/MiniMaxAI/MiniMax-M2
+            res = "minimax-m2"
