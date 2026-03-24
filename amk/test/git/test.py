16f1a1d1881ba6efe69963a74589d468abb55c0a
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-24 18:34:36 +0300

    amk agent dev. sys. 1188 - `tester agent scripts/general agents` (1188/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1185,3 +1185,4 @@ class TextModel(ModelBase):
             res = "llama4"
         if chkhsh == "0e9433cbbb161f89e264eb32e8e64bfe69e834973ffca5d41d3948a604a3e2a3":
             # ref: https://huggingface.co/mistral-community/pixtral-12b
+            res = "pixtral"
