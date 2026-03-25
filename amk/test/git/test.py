c79710a5c7c8ec60a84a59ed2760fe6380703e87
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-25 21:40:47 +0300

    amk agent dev. sys. 1286 - `tester agent scripts/general agents` (1286/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1283,3 +1283,4 @@ class TextModel(ModelBase):
         reverse_vocab = {id_ : encoded_tok for encoded_tok, id_ in {**vocab, **added_vocab}.items()}
 
         for i in range(vocab_size):
+            if i not in reverse_vocab:
