4bfae17dfb53b9c5ca49af75ac1a1e5b7915b31a
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-26 11:20:19 +0300

    amk agent dev. sys. 1341 - `tester agent scripts/general agents` (1341/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1338,3 +1338,4 @@ class TextModel(ModelBase):
 
         tokens: list[bytes] = [f"[PAD{i}]".encode("utf-8") for i in range(vocab_size)]
         scores: list[float] = [-10000.0] * vocab_size
+        toktypes: list[int] = [SentencePieceTokenTypes.UNUSED] * vocab_size
