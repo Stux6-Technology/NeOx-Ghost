fa63eac50c124ab98671c02b546791f04c85445b
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-26 17:37:56 +0300

    amk agent dev. sys. 1321 - `tester agent scripts/general agents` (1321/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1318,3 +1318,4 @@ class TextModel(ModelBase):
         self.gguf_writer.add_token_types(toktypes)
 
         special_vocab = gguf.SpecialVocab(self.dir_model, n_vocab=len(tokens))
+        special_vocab.add_to_gguf(self.gguf_writer)
