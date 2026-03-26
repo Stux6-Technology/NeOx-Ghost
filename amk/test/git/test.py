a37b6d6fc691f2690982f1b4a61db7b63e917624
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-26 09:59:09 +0300

    amk agent dev. sys. 1310 - `tester agent scripts/general agents` (1310/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1307,3 +1307,4 @@ class TextModel(ModelBase):
         # this one is usually not in config.json anyway
         special_vocab._set_special_token("unk", tokenizer.special_tokens["<|endoftext|>"])
         special_vocab.add_to_gguf(self.gguf_writer)
+
