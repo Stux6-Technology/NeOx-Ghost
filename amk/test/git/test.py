df27c71cf85f524b385be7ba2b19a5a4d9b7ac2a
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-28 22:33:44 +0300

    amk agent dev. sys. 1428 - `tester agent scripts/general agents` (1428/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1425,3 +1425,4 @@ class TextModel(ModelBase):
 
         self.gguf_writer.add_tokenizer_model("llama")
         self.gguf_writer.add_tokenizer_pre("default")
+        self.gguf_writer.add_token_list(tokens)
