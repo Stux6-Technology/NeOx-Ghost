734f8aae002314083c72b5cbd880a6fbf8cc7b63
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-29 15:34:42 +0300

    amk agent dev. sys. 1565 - `tester agent scripts/general agents` (1565/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1562,3 +1562,4 @@ class TextModel(ModelBase):
         special_vocab._set_special_token("unk", tokenizer.get_added_vocab()["<|endoftext|>"])
         special_vocab._set_special_token("bos", tokenizer.get_added_vocab()["<|endoftext|>"])
         special_vocab.add_to_gguf(self.gguf_writer)
+
