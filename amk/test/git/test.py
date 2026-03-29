fa63f717d881a4d46cbf07e7a4f0c900c304deb4
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-29 20:05:56 +0300

    amk agent dev. sys. 1525 - `tester agent scripts/general agents` (1525/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1522,3 +1522,4 @@ class TextModel(ModelBase):
         if (field := vocab_reader.get_field(gguf.Keys.Tokenizer.ADD_EOS)) is not None:
             self.gguf_writer.add_add_eos_token(field.parts[-1].tolist()[0])
 
+    def _try_set_pooling_type(self) -> None:
