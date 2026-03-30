adac963e96e3e7dea40f23f10bd4f33d2e9b463c
Alperen ERKAN <erkanalperen54@gmail.com>
2026-03-30 16:04:10 +0300

    amk agent dev. sys. 1598 - `tester agent scripts/general agents` (1598/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1595,3 +1595,4 @@ class TextModel(ModelBase):
                         if previous_token != token:
                             logger.info(f"{repr(previous_token)} is encoded and decoded back to {repr(token)} using AutoTokenizer")
 
+                    if added_tokens_decoder[i].special or self.does_token_look_special(token):
