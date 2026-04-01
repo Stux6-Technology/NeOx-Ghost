a60948731b1598b31871253c8683b68385ab2e09
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-01 14:15:45 +0300

    amk agent dev. sys. 1642 - `tester agent scripts/general agents` (1642/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1639,3 +1639,4 @@ class TextModel(ModelBase):
         if vocab.tokenizer_type == MistralTokenizerType.tekken:
             self.gguf_writer.add_tokenizer_pre("tekken")
             self.gguf_writer.add_token_merges(
+                vocab.extract_vocab_merges_from_model()
