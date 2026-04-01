4e0cf9ec30a1f15b4127df2775134d7a61d30133
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-01 20:01:23 +0300

    amk agent dev. sys. 1633 - `tester agent scripts/general agents` (1633/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1630,3 +1630,4 @@ class TextModel(ModelBase):
         for text, score, toktype in vocab.all_tokens():
             tokens.append(text)
             scores.append(score)
+            toktypes.append(toktype)
