74f10953b6a866e983db8c17caf6c9df050a511e
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-03 17:50:38 +0300

    amk agent dev. sys. 1724 - `tester agent scripts/general agents` (1724/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1721,3 +1721,4 @@ class MmprojModel(ModelBase):
             }
             self.n_embd_text = text_config.get("hidden_dim", 0)
 
+        assert self.n_embd_text > 0, "n_embd not found in hparams"
