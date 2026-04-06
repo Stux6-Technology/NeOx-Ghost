bb39b536723a769c0eab8f0724e2ce9d95565c93
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-06 21:12:13 +0300

    amk agent dev. sys. 1834 - `tester agent scripts/general agents` (1834/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1831,3 +1831,4 @@ class MmprojModel(ModelBase):
     def find_aparam(self, keys: Iterable[str], optional: bool = False) -> Any:
         assert self.hparams_audio is not None
         return self._find_param(self.hparams_audio, keys, optional)
+
