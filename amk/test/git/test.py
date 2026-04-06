7a5f5a6ecbb19f784c6a022fe26a4622d5a6cc17
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-06 10:23:52 +0300

    amk agent dev. sys. 1882 - `tester agent scripts/general agents` (1882/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1879,3 +1879,4 @@ class GPTNeoXModel(TextModel):
             data_torch = torch.cat(
                 (
                     qkv_weights[:, 0, :, :].reshape((-1, n_embed)),
+                    qkv_weights[:, 1, :, :].reshape((-1, n_embed)),
