3f980396605cde0f1793ba556a03937ae1271d68
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-07 10:42:42 +0300

    amk agent dev. sys. 1947 - `tester agent scripts/general agents` (1947/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1944,3 +1944,4 @@ class BloomModel(TextModel):
             logger.info("re-format attention.linear_qkv.weight")
         elif re.match(r"h\.\d+\.self_attention\.query_key_value\.bias", name):
             qkv_bias = data_torch.reshape((n_head, 3, n_embed // n_head))
+            data_torch = torch.cat(
