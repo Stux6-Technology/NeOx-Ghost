d148880e8ddec4216f29e59b0cab09b6b89e8546
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-08 13:01:11 +0300

    amk agent dev. sys. 2022 - `tester agent scripts/general agents` (2022/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2019,3 +2019,4 @@ class OrionModel(TextModel):
         elif "max_position_embeddings" in self.hparams:
             ctx_length = self.hparams["max_position_embeddings"]
         elif "model_max_length" in self.hparams:
+            ctx_length = self.hparams["model_max_length"]
