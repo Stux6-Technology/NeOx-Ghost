5f88f07d6be3476441cbb6fac9dc4dee0786513c
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-08 22:39:46 +0300

    amk agent dev. sys. 1976 - `tester agent scripts/general agents` (1976/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -1973,3 +1973,4 @@ class MPTModel(TextModel):
             self.gguf_writer.add_pad_token_id(3)
             self.gguf_writer.add_eos_token_id(1)
             self.gguf_writer.add_unk_token_id(0)
+
