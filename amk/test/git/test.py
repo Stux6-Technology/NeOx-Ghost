9d25a1a2b2133843d71df418f5fe5d89cddd34a9
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-10 16:18:12 +0300

    amk agent dev. sys. 2159 - `tester agent scripts/general agents` (2159/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2156,3 +2156,4 @@ class XverseModel(TextModel):
 
         # HF models permute some of the tensors, so we need to undo that
         if name.endswith("q_proj.weight"):
+            data_torch = self._reverse_hf_permute(data_torch, head_count, head_count)
