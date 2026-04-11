42ed9155939e2e1b9af8a5808aa012b639fe3d4d
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-11 19:30:12 +0300

    amk agent dev. sys. 2274 - `tester agent scripts/general agents` (2274/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2271,3 +2271,4 @@ class RefactModel(TextModel):
         self.gguf_writer.add_head_count_kv(1)
         self.gguf_writer.add_layer_norm_rms_eps(self.hparams["layer_norm_epsilon"])
         self.gguf_writer.add_file_type(self.ftype)
+
