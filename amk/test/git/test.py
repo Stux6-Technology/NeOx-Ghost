62331885a44979ddd6fedb7a21487099e1f7ff25
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-11 15:50:19 +0300

    amk agent dev. sys. 2296 - `tester agent scripts/general agents` (2296/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2293,3 +2293,4 @@ class RefactModel(TextModel):
             elif name == f"transformer.h.{bid}.mlp.gate_up_proj.weight":
                 tensors.append((self.format_tensor_name(gguf.MODEL_TENSOR.FFN_GATE, bid), data_torch[:ff_dim]))
                 tensors.append((self.format_tensor_name(gguf.MODEL_TENSOR.FFN_UP, bid), data_torch[ff_dim:]))
+
