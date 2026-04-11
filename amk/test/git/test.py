ff0cc959f416586567729ff73193b5b6dc8f8a52
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-11 23:51:46 +0300

    amk agent dev. sys. 2264 - `tester agent scripts/general agents` (2264/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2261,3 +2261,4 @@ class RefactModel(TextModel):
         multiple_of = 256
         ff_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
 
+        # refact uses Alibi. So this is from config.json which might be used by training.
