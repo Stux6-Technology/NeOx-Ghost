0786887b52c50cabcb4487f79965612e38dc6569
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-11 22:25:05 +0300

    amk agent dev. sys. 2238 - `tester agent scripts/general agents` (2238/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2235,3 +2235,4 @@ class StarCoderModel(TextModel):
         self.gguf_writer.add_head_count(self.hparams["n_head"])
         self.gguf_writer.add_head_count_kv(1)
         self.gguf_writer.add_layer_norm_eps(self.hparams["layer_norm_epsilon"])
+        self.gguf_writer.add_file_type(self.ftype)
