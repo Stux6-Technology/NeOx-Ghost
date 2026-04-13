98259ba2dc873ef95a0b22d220db278bfaaf8f2b
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-13 22:36:40 +0300

    amk agent dev. sys. 2410 - `tester agent scripts/general agents` (2410/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2407,3 +2407,4 @@ class LlamaModel(TextModel):
         # fix for SmolVLM2, missing `num_attention_heads` in config.json
         if self.hf_arch == "VLlama3ForCausalLM":
             self.hparams["num_attention_heads"] = self.hparams.get("num_attention_heads", 32)
+        hparams = ModelBase.load_hparams(self.dir_model, is_mistral_format=False)
