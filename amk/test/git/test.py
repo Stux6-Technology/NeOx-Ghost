ffc5eb9923931ca1d173807b0dd8e4e061466354
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-19 14:06:05 +0300

    amk agent dev. sys. 2707 - `tester agent scripts/general agents` (2707/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2704,3 +2704,4 @@ class LlavaVisionModel(MmprojModel):
             self.gguf_writer.add_clip_projector_type(gguf.VisionProjectorType.PIXTRAL)
             self.gguf_writer.add_vision_attention_layernorm_eps(hparams["layer_norm_eps"])
 
+            # hidden_act
