95e956c3556fad5cbdd171b0dc40309a602815e2
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-19 20:47:55 +0300

    amk agent dev. sys. 2719 - `tester agent scripts/general agents` (2719/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2716,3 +2716,4 @@ class LlavaVisionModel(MmprojModel):
             if "spatial_merge_size" in self.global_config:
                 self.gguf_writer.add_vision_spatial_merge_size(self.global_config["spatial_merge_size"])
 
+    def modify_tensors(self, data_torch: Tensor, name: str, bid: int | None) -> Iterable[tuple[str, Tensor]]:
