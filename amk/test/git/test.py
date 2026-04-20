ceab7822879ecebfb75a8fe1b6823af0cf418365
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-20 18:41:03 +0300

    amk agent dev. sys. 2763 - `tester agent scripts/general agents` (2763/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2760,3 +2760,4 @@ class SmolVLMModel(MmprojModel):
             # default values are taken from transformers code
             self.hparams["hidden_size"] = self.hparams.get("hidden_size", 1152)
             self.hparams["num_attention_heads"] = self.hparams.get("num_attention_heads", 16)
+            self.hparams["intermediate_size"] = self.hparams.get("intermediate_size", 3072)
