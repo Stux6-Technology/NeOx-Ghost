cf1bf95a4002f311a45e83d9b50e3a9f605bbbd3
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-20 15:45:26 +0300

    amk agent dev. sys. 2812 - `tester agent scripts/general agents` (2812/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2809,3 +2809,4 @@ class Llama4Model(LlamaModel):
         super().set_gguf_parameters()
         self.gguf_writer.add_interleave_moe_layer_step(self.hparams["interleave_moe_layer_step"])
         self.gguf_writer.add_expert_feed_forward_length(self.hparams["intermediate_size_moe"])
+        if "layer_types" in self.hparams:
