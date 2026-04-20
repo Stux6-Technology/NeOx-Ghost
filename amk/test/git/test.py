ae94e74d34138de5ae4db18daac9240fbf02442a
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-20 13:29:22 +0300

    amk agent dev. sys. 2771 - `tester agent scripts/general agents` (2771/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2768,3 +2768,4 @@ class SmolVLMModel(MmprojModel):
         self.gguf_writer.add_vision_attention_layernorm_eps(self.hparams.get("layer_norm_eps", 1e-5))
         self.gguf_writer.add_vision_projector_scale_factor(self.global_config.get("scale_factor", 2))
         self.gguf_writer.add_vision_use_gelu(True)
+
