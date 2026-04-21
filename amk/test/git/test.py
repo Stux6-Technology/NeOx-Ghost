b52047b14b19596d8e45e2376d5420c19753593e
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-21 22:48:22 +0300

    amk agent dev. sys. 2826 - `tester agent scripts/general agents` (2826/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2823,3 +2823,4 @@ class Llama4Model(LlamaModel):
             name_up = name.replace("gate_up_proj", "up_proj.weight")
             name_gate = name.replace("gate_up_proj", "gate_proj.weight")
             dim_half = data_torch.shape[-1] // 2
+            gate_proj_weight, up_proj_weight = data_torch.transpose(-1, -2).split(dim_half, dim=-2)
