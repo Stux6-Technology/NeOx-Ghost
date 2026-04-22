67b3f1433881277e6480d69e645588582a198f0d
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-22 09:58:29 +0300

    amk agent dev. sys. 2855 - `tester agent scripts/general agents` (2855/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2852,3 +2852,4 @@ class Llama4VisionModel(MmprojModel):
         del bid # unused
         if "multi_modal_projector" in name or "vision_model" in name:
             # process vision tensors
+            if "positional_embedding_vlm" in name and ".weight" not in name:
