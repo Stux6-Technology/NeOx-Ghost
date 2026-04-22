c4b7d89c97770af68f25472a8dc49fdfa0d90eaf
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-22 12:37:41 +0300

    amk agent dev. sys. 2858 - `tester agent scripts/general agents` (2858/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -2855,3 +2855,4 @@ class Llama4VisionModel(MmprojModel):
             if "positional_embedding_vlm" in name and ".weight" not in name:
                 name += ".weight"
             if "multi_modal_projector.linear_1" in name:
+                # despite the name with number postfix, this is a single fully connected layer
