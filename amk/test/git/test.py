22b995e199c4afb4a0cef5716b697d05c2f56e64
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-24 19:14:32 +0300

    amk agent dev. sys. 3063 - `tester agent scripts/general agents` (3063/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -3060,3 +3060,4 @@ class DeciModel(TextModel):
 
                 yield (self.format_tensor_name(gguf.MODEL_TENSOR.ROPE_FREQS), torch.tensor(rope_factors, dtype=torch.float32))
 
+    def prepare_tensors(self):
