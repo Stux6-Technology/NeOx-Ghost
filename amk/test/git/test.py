4a83667e3f4393362c2caac0ffa5a29ff62b99a4
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-24 22:42:17 +0300

    amk agent dev. sys. 3046 - `tester agent scripts/general agents` (3046/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -3043,3 +3043,4 @@ class DeciModel(TextModel):
                 high_freq_factor = rope_params.get("high_freq_factor", 4.0)
                 old_context_len = self.hparams.get("original_max_position_embeddings", 8192)
 
+                low_freq_wavelen = old_context_len / low_freq_factor
