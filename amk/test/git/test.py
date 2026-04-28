7a23562c721145f9ba1d6e2c2cd579197bf8a8f5
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-28 12:13:01 +0300

    amk agent dev. sys. 3179 - `tester agent scripts/general agents` (3179/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -3176,3 +3176,4 @@ class GrokModel(TextModel):
             # concatenate split tensors
             if name in self._experts[bid]:
                 self._cur_expert = name
+                self._experts[bid][name].append(data_torch)
