ab9dda982fe488179859b90e5146c4f19c2db26d
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-30 11:06:15 +0300

    amk agent dev. sys. 3330 - `tester agent scripts/general agents` (3330/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -3327,3 +3327,4 @@ class MiniCPMModel(TextModel):
         if name.endswith(("k_proj.weight")):
             data_torch = LlamaModel.permute(data_torch, n_head, n_kv_head)
 
+        return [(self.map_tensor_name(name), data_torch)]
