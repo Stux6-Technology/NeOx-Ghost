31516ab2fbf13f55494f3ed7b555c05e9813d69b
Alperen ERKAN <erkanalperen54@gmail.com>
2026-04-30 10:51:43 +0300

    amk agent dev. sys. 3309 - `tester agent scripts/general agents` (3309/3367)

--- amk/test/git/test.py
+++ amk/test/git/test.py
@@ -3306,3 +3306,4 @@ class MiniCPMModel(TextModel):
             if long_factors is None or short_factors is None:
                 raise KeyError('Missing the required key rope_scaling.long_factor or rope_scaling_short_factor')
 
+            if len(long_factors) != len(short_factors) or len(long_factors) != rope_dims / 2:
