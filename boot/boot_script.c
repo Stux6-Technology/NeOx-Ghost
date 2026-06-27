f77651aca586a9d00e78b205933023a4483a231b
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-27 12:43:08 +0300

    boot 2 - symbol creat / include update

--- boot/boot_script.c
+++ boot/boot_script.c
@@ -3,3 +3,32 @@
 /* Alperen ERKAN - (Stux6 Technology Organization)
    erkanalperen54 [at] gmail.com - */
 
+#include <mach/mach_types.h>
+#if !KERNEL || OSKIT_MACH
+#include <string.h>
+#include <stdio.h>
+#endif
+#include "boot_script.h"
+
+/* This structure describes a sysmbol. */
+struct sym
+{
+	/* symbol name. */
+	const char *name;
+
+	/* types of volue returned by function. */
+	int type;
+
+	/* symbol value. */
+	intptr_t val;
+
+	/* For function symbols, type of volue returned by function. */
+	int ret_type;
+
+	/* Note:
+	For function symbols: if set, execute function at the time 
+	of command execution, not furing parsing. A function with
+	this field set must also have `no_arg` set. Also, the functions's
+	`val` atgument will always be NULL.*/
+	int run_on_exec;
+}
