0e7343705e9679afd3a7fb810efd7172ff55dc9c
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-29 21:59:36 +0300

    amk 382 - `amk-test.c` (step 382/483)

--- amk/test/amk-test.c
+++ amk/test/amk-test.c
@@ -379,3 +379,4 @@ lookup_host (struct hostmux *mux, const char *host, struct node **node)
 /* This should sync the entire remote filesystem.  If WAIT is set, return
    only after sync is completely finished.  */
 error_t
+netfs_attempt_syncfs (struct iouser *cred, int wait)
