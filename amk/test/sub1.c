8d9f3e91a18c2c8e0164db0038589a048eeca748
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-29 21:54:59 +0300

    sub 59 - `sub1.c` (step 59/123)

--- amk/test/sub1.c
+++ amk/test/sub1.c
@@ -56,3 +56,4 @@ netfs_attempt_create_file (struct iouser *user, struct node *dir,
 error_t
 netfs_check_open_permissions (struct iouser *user, struct node *node,
 			      int flags, int newnode)
+{
