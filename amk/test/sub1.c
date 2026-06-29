7f046af3b807f0bb2672e2d9f51163cceea72aff
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-29 21:54:53 +0300

    sub 7 - `sub1.c` (step 7/123)

--- amk/test/sub1.c
+++ amk/test/sub1.c
@@ -4,3 +4,4 @@
  *   DEVELOPER: Stux6 Tech. Lead Eng. Alperen ERKAN <erkanalperen54 [at] gmail.com> 
  *              or <stux6.team@gmail.com>
  * ==========================================================================
+ *   LICENSE SUMMARY (STUX6 GENERAL PRIVATE PROJECT LICENSE - SGPPL-v1.0)
