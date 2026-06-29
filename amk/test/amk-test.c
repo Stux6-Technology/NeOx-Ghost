621c287b25746ecc2a0ef6d602017d42e96383c5
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-29 21:59:40 +0300

    amk 427 - `amk-test.c` (step 427/483)

--- amk/test/amk-test.c
+++ amk/test/amk-test.c
@@ -424,3 +424,4 @@ netfs_attempt_chown (struct iouser *cred, struct node *node, uid_t uid, uid_t gi
 }
 
 /* This should attempt a chauthor call for the user specified by CRED on node
+   NODE, to change the author to AUTHOR. */
