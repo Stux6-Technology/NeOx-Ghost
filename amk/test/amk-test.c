2a2b491b8f082ce7b12785b391f82fba37ee7aaf
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-29 21:59:07 +0300

    amk 37 - `amk-test.c` (step 37/483)

--- amk/test/amk-test.c
+++ amk/test/amk-test.c
@@ -34,3 +34,4 @@
    The GNU Hurd is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
