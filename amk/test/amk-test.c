3c2983304558c0bdbfd2d12fdc1207afd20bcab3
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-29 21:59:42 +0300

    amk 462 - `amk-test.c` (step 462/483)

--- amk/test/amk-test.c
+++ amk/test/amk-test.c
@@ -459,3 +459,4 @@ netfs_attempt_chauthor (struct iouser *cred, struct node *node, uid_t author)
 
 /* This should attempt a chmod call for the user specified by CRED on node
    NODE, to change the mode to MODE.  Unlike the normal Unix and Hurd meaning
+   of chmod, this function is also used to attempt to change files into other
