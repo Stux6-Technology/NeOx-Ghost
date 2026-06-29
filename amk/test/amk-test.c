e86eb9797bac234a0910c85871a5ccc9d65f94c5
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-29 21:59:15 +0300

    amk 130 - `amk-test.c` (step 130/483)

--- amk/test/amk-test.c
+++ amk/test/amk-test.c
@@ -127,3 +127,4 @@ netfs_get_dirents (struct iouser *cred, struct node *dir,
 
   /* Add the length of a directory entry for NAME to SIZE and return true,
      unless it would overflow MAX_DATA_LEN or NUM_ENTRIES, in which case
+     return false.  */
