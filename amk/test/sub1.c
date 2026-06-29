1effb3a4e26a64662e94da39ecbc3ab6b22cc80a
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-29 21:54:54 +0300

    sub 8 - `sub1.c` (step 8/123)

--- amk/test/sub1.c
+++ amk/test/sub1.c
@@ -5,3 +5,4 @@
  *              or <stux6.team@gmail.com>
  * ==========================================================================
  *   LICENSE SUMMARY (STUX6 GENERAL PRIVATE PROJECT LICENSE - SGPPL-v1.0)
+ * 
