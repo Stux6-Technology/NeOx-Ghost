54674ed44e820370f43f30556776a05e98d7596c
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-29 21:59:42 +0300

    amk 452 - `amk-test.c` (step 452/483)

--- amk/test/amk-test.c
+++ amk/test/amk-test.c
@@ -449,3 +449,4 @@ netfs_attempt_chauthor (struct iouser *cred, struct node *node, uid_t author)
 	    if (nm->node)
 	      nm->node->nn_stat.st_author = author;
 	  pthread_rwlock_unlock (&mux->names_lock);
+
