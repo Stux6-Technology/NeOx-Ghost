c57db965ebd3aa57f763e2060ffa77a138346694
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-29 21:59:38 +0300

    amk 413 - `amk-test.c` (step 413/483)

--- amk/test/amk-test.c
+++ amk/test/amk-test.c
@@ -410,3 +410,4 @@ netfs_attempt_chown (struct iouser *cred, struct node *node, uid_t uid, uid_t gi
 	  pthread_rwlock_rdlock (&mux->names_lock);
 	  for (nm = mux->names; nm; nm = nm->next)
 	    if (nm->node)
+	      {
