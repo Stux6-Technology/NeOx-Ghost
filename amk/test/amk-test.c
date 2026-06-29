6e674f0a71393c64efcf4928c5e7929fdbe5206f
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-29 21:59:41 +0300

    amk 444 - `amk-test.c` (step 444/483)

--- amk/test/amk-test.c
+++ amk/test/amk-test.c
@@ -441,3 +441,4 @@ netfs_attempt_chauthor (struct iouser *cred, struct node *node, uid_t author)
 
 	  /* Change NODE's owner.  */
 	  mux->stat_template.st_author = author;
+	  node->nn_stat.st_author = author;
