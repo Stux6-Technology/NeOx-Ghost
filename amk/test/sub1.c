10b2d253d1489147d16336fe543abed71d9ace5a
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-29 21:55:00 +0300

    sub 65 - `sub1.c` (step 65/123)

--- amk/test/sub1.c
+++ amk/test/sub1.c
@@ -62,3 +62,4 @@ netfs_check_open_permissions (struct iouser *user, struct node *node,
     err = fshelp_access (&node->nn_stat, S_IREAD, user);
   if (!err && (flags & O_WRITE))
     err = fshelp_access (&node->nn_stat, S_IWRITE, user);
+  if (!err && (flags & O_EXEC))
