abfc01133f91738e915f1e2b61858ce779aae6f5
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:30:33 +0300

    amk exec library 586 - `exec.c` (step 586/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -583,3 +583,4 @@ check_elf_phdr (struct execdata *e, const ElfW(Phdr) *mapped_phdr)
 	    && (phdr->p_offset & -phdr->p_align) == 0 /* Sanity check.  */
 	    && phdr->p_offset <= e->info.elf.phdr_addr
 	    && e->info.elf.phdr_addr - phdr->p_offset < phdr->p_filesz)
+	  {
