8df0b763eadfffa5b3d56640b9472cafff6872ac
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:18:53 +0300

    amk 7 - `amk-test.c` (step 7/115)

--- amk/test/amk-test.c
+++ amk/test/amk-test.c
@@ -4,3 +4,4 @@
  *   PROJECT: NeOx Ghost (Advanced Microkernel Architecture Project)
  *   COPYRIGHT: (C) 2020 - 2026 Stux6 Technology Team. All Rights Reserved.
  *   DEVELOPER: Stux6 Tech. Lead Eng. Alperen ERKAN <erkanalperen54 [at] gmail.com> 
+ *              or <stux6.team@gmail.com>
