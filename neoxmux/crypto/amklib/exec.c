999ac3a825a2da15841cd089bdc141914f947daf
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:29:41 +0300

    amk exec library 138 - `exec.c` (step 138/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -135,3 +135,4 @@ load_section (void *section, struct execdata *u)
 	    /* Data is already in memory; write it into the task.  */
 	    write_to_task (&mapstart, filesz - (mapstart - addr), vm_prot,
 			   (vm_address_t) SECTION_CONTENTS
+			   + (mapstart - addr));
