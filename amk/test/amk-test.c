61d9dbb012cd60c06eca8aaef5bbe819b7f16bb0
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:18:55 +0300

    amk 17 - `amk-test.c` (step 17/115)

--- amk/test/amk-test.c
+++ amk/test/amk-test.c
@@ -14,3 +14,4 @@
  *      for personal research and local laboratory development only.
  *   3. ANY DISTRIBUTION, FORKING, OR RE-PUBLISHING ON ANY INTERNET PLATFORM 
  *      (INCLUDING GITHUB, GITLAB, BITBUCKET) IS STRICTLY PROHIBITED.
+ *   4. Commercial enterprise, government network, or military deployment 
