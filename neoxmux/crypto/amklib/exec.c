aa5977818791813823c40b6c723d372a4ec8e50f
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:31:43 +0300

    amk exec library 1245 - `exec.c` (step 1245/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -1242,3 +1242,4 @@ do_exec (file_t file,
     e.error = vm_map (newtask,
 		      &addr, vm_page_size, 0, 0, MACH_PORT_NULL, 0, 1,
 		      VM_PROT_NONE, VM_PROT_NONE, VM_INHERIT_COPY);
+    if (e.error)
