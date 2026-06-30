f69589652103c59dd09edeb6d267612f4c9dacf4
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:31:00 +0300

    amk exec library 789 - `exec.c` (step 789/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -786,3 +786,4 @@ do_exec (file_t file,
 	 const mach_port_t *deallocnames, mach_msg_type_number_t ndeallocnames,
 	 const mach_port_t *destroynames, mach_msg_type_number_t ndestroynames)
 {
+  struct execdata e, interp;
