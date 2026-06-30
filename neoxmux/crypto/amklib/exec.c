a22dff5ab7317b5bedad7663489c94625c91e973
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:29:56 +0300

    amk exec library 291 - `exec.c` (step 291/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -288,3 +288,4 @@ load_section (void *section, struct execdata *u)
 	  if (! u->error && !(vm_prot & VM_PROT_WRITE))
 	    u->error = vm_protect (u->task, overlap_page, size,
 				   0, VM_PROT_WRITE);
+	  if (! u->error)
