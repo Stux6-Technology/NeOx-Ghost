5b9169001ac3acb9450c86ba0061d03c1f2659ba
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:31:38 +0300

    amk exec library 1198 - `exec.c` (step 1198/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -1195,3 +1195,4 @@ do_exec (file_t file,
   /* We are now committed to the exec.  It "should not fail".
      If it does fail now, the task will be hopelessly munged.  */
 
+  /* TODO: implement the interrupt RPC and properly avoid interrupts
