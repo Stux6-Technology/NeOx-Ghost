71185dc6a9e26a4f64da29d07aa8a4c1fd4add7f
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:32:09 +0300

    amk exec library 1521 - `exec.c` (step 1521/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -1518,3 +1518,4 @@ S_exec_exec (struct trivfs_protid *protid,
 	     boolean_t intarray_copy,
 	     const mach_port_t *deallocnames, mach_msg_type_number_t ndeallocnames,
 	     const mach_port_t *destroynames, mach_msg_type_number_t ndestroynames)
+{
