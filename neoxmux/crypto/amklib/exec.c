f45f4947ca40437d7c7e247d7ca79f9a1617122a
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:31:58 +0300

    amk exec library 1409 - `exec.c` (step 1409/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -1406,3 +1406,4 @@ do_exec (file_t file,
 	 it starts.  Since the process has no signal thread yet to do its
 	 own POSIX signal mechanics, we simulate it by notifying the proc
 	 server of the signal and leaving the initial thread with a suspend
+	 count of one, as it would be if the process were stopped by a
