da3dd8a3edd970b2156ee00c5e282104fd1c0126
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:29:57 +0300

    amk exec library 314 - `exec.c` (step 314/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -311,3 +311,4 @@ map (struct execdata *e, off_t posn, size_t len)
 
   if ((map_filepos (e) & ~(map_vsize (e) - 1)) == (posn & ~(map_vsize (e) - 1))
       && posn + len - map_filepos (e) <= map_fsize (e))
+    /* The current mapping window covers it.  */
