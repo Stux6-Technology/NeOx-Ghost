05c310f1e254de72062ff1515931abbe1e8cd953
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:30:57 +0300

    amk exec library 763 - `exec.c` (step 763/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -760,3 +760,4 @@ servercopy (void *arg, mach_msg_type_number_t argsize, boolean_t argcopy,
   /* ARG came in-line, so we must copy it.  */
   void *copy;
   copy = mmap (0, argsize, PROT_READ|PROT_WRITE, MAP_ANON, 0, 0);
+  if (copy == MAP_FAILED)
