f84bedfe24680ef415158792b16dae32b24076f5
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:31:45 +0300

    amk exec library 1273 - `exec.c` (step 1273/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -1270,3 +1270,4 @@ do_exec (file_t file,
      program's startup code will need to know.  We do this after loading
      the image so that a load-anywhere image gets the adjusted addresses.  */
     if (e.info.elf.phdr_addr != 0)
+      {
