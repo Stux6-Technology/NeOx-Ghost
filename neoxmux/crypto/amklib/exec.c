a12adfbffef11a5f5070a1ad2d94eff5456664ae
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:30:53 +0300

    amk exec library 722 - `exec.c` (step 722/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -719,3 +719,4 @@ load (task_t usertask, struct execdata *e)
 
       /* Ask the kernel to find this much contiguous memory.  */
       e->error = vm_allocate (usertask, &anywhere_start, mapping_size, 1);
+      if (e->error)
