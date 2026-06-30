b5f72e0648f7f2d74152e7f45e5ccb58d36e3ade
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:30:30 +0300

    amk exec library 561 - `exec.c` (step 561/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -558,3 +558,4 @@ check_elf_phdr (struct execdata *e, const ElfW(Phdr) *mapped_phdr)
 	  e->info.elf.phnum * sizeof (ElfW(Phdr)));
 
   /* Default state if we do not see PT_GNU_STACK telling us what to do.
+     Executable stack is the compatible default.
