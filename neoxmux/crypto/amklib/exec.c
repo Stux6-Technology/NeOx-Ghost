e4e8b105d233f33c5d28e66a9a167a339bec6506
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:29:53 +0300

    amk exec library 266 - `exec.c` (step 266/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -263,3 +263,4 @@ load_section (void *section, struct execdata *u)
 	     Allocate all the pages that start inside the section.  */
 	  u->error = vm_map (u->task, &mapstart, memsz - (mapstart - addr),
 			     mask, 0, MACH_PORT_NULL, 0, 1,
+			     vm_prot, VM_PROT_ALL, VM_INHERIT_COPY);
