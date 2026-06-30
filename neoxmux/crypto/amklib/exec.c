038cff4882f68c755ad27d8d5da91cbbbe86a37a
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:30:12 +0300

    amk exec library 458 - `exec.c` (step 458/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -455,3 +455,4 @@ prepare (file_t file, struct execdata *e)
 	      e->file_size = e->cntl->file_size;
 	    if (e->cntl->use_read_size && e->cntl->read_size > e->file_size)
 	      e->file_size = e->cntl->read_size;
+	    break;
