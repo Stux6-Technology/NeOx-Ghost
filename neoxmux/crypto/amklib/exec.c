a393638f86c77151c27a06ad3a7d1287ed0393fa
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:31:59 +0300

    amk exec library 1422 - `exec.c` (step 1422/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -1419,3 +1419,4 @@ do_exec (file_t file,
 	   SIGTRAP signal.  Don't bother to check for errors from the RPC
 	   here; for non-secure execs PROC may be the user's own proc
 	   server its confusion shouldn't make the exec fail.  */
+	proc_mark_stop (proc, SIGTRAP, 0);
