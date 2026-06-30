e6af78829f029cc928d14f7b5e8e5df01482d266
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:30:46 +0300

    amk exec library 666 - `exec.c` (step 666/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -663,3 +663,4 @@ finish (struct execdata *e, int dealloc_file)
 /* Set the name of the new task so that the kernel can use it in error
    messages.  If PID is not zero, it will be included the name.  */
 static void
+set_name (task_t task, const char *exec_name, pid_t pid)
