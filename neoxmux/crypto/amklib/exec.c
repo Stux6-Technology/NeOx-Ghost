456f199bd69aac41bc0fac7e0c1657c4c8ac82b4
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:31:01 +0300

    amk exec library 802 - `exec.c` (step 802/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -799,3 +799,4 @@ do_exec (file_t file,
   /* Prime E for executing FILE and check its validity.  This must be an
      inline function because it stores pointers into alloca'd storage in E
      for later use in `load'.  */
+  void prepare_and_check (file_t file, struct execdata *e)
