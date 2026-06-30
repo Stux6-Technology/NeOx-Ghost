9d47d5ab97358b0216d2ca5cd2ff9cd192cfd3ef
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:32:13 +0300

    amk exec library 1571 - `exec.c` (step 1571/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -1568,3 +1568,4 @@ S_exec_exec_paths (struct trivfs_protid *protid,
   /* For some of the arrays, we deallocate by hand, thus dropping const.  */
 
   return do_exec (file, oldtask, flags, path, abspath,
+		  (char*) argv, argvlen, argv_copy,
