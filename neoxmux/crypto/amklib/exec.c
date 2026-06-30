b08eb124b0df73fc7dd7e1445f7652c622a4ea05
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:32:05 +0300

    amk exec library 1482 - `exec.c` (step 1482/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -1479,3 +1479,4 @@ do_exec (file_t file,
       else
 	/* Resume the task, it is ready to run the new program.  */
 	task_resume (oldtask);
+      /* Deallocate the right to the new task we created.  */
