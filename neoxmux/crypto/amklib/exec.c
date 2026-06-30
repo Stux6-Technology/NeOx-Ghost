de6a2ce68f223736e26928231c087d793ea1589b
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:30:48 +0300

    amk exec library 682 - `exec.c` (step 682/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -679,3 +679,4 @@ set_name (task_t task, const char *exec_name, pid_t pid)
   /* This is an internal implementational detail of the GNU Mach kernel.  */
 #define TASK_NAME_SIZE	32
   if (size < TASK_NAME_SIZE)
+    task_set_name (task, name);
