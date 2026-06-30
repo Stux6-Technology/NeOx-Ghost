c74b3756b3a8e8262d4d80b84dfc9d023c3447dc
Alperen ERKAN <erkanalperen54@gmail.com>
2026-06-30 11:31:24 +0300

    amk exec library 1045 - `exec.c` (step 1045/1685)

--- neoxmux/crypto/amklib/exec.c
+++ neoxmux/crypto/amklib/exec.c
@@ -1042,3 +1042,4 @@ do_exec (file_t file,
 	    /* Allocate a new vector that is big enough.  */
 	    boot->intarray = mmap (0, INIT_INT_MAX * sizeof (int),
 				   PROT_READ|PROT_WRITE, MAP_ANON, 0, 0);
+	    memcpy (boot->intarray, intarray, nints * sizeof (int));
