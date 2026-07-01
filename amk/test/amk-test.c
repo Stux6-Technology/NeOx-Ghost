d48e788dc010b6685b7e5ab6f1320c7f28c66781
Alperen ERKAN <erkanalperen54@gmail.com>
2026-07-01 10:19:36 +0300

    amk 4 - `amk-test.c` (step 4/115)

--- amk/test/amk-test.c
+++ amk/test/amk-test.c
@@ -1,3 +1,4 @@
 /* Root neoxmux amk protocol node */
 
 /* ==========================================================================
+ *   PROJECT: NeOx Ghost (Advanced Microkernel Architecture Project)
